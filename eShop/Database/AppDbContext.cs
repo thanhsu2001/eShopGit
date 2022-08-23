5eccca1b42ee19d7435f04b3d99d5e220df282ba
ThanhSu <vothanhsu22022001@gmail.com>
2022-08-23 19:50:54 +0700

    code cai dat

---
+++ eShop/Database/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using eShop.Database.Configs;
+using eShop.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShop.Database
+{
+	public class AppDbContext:DbContext
+	{
+		public DbSet<ProductCategory> ProductCategories { get; set; }
+		public DbSet<Product> Products { get; set; }
+		public AppDbContext(DbContextOptions options) : base(options)
+		{
+
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfiguration(new ProductCategoryConfig());
+			modelBuilder.ApplyConfiguration(new ProductConfig());
+		}
+	}
+}
